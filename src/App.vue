5b22c98eb7015820661b1b9caf44c8a8e4da4932
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-20 12:09:58 -0700

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
 
-  <h2 v-for="(name,index) in names" :key="name">{{index}} {{name}}</h2>
-  <h2 v-for="person in fullNames" :key="person.first"> {{person.first}} {{person.last}} </h2>
-  <br />
-  <div v-for="actor in actors" :key="actor.name"> 
-    <h2>{{actor.name}} </h2>
-    <h3 v-for="movie in actor.movies" :key="movie"> {{movie}} </h3>
-  </div>
-
-  <h2 v-for="(value,key,index) in myInfo" :key="value" >{{index}}  {{key}} {{ value }}</h2>
-  <hr/>
-  <template v-for="name in names" :key="name"> 
-    <h2> {{name}} </h2>
-    <hr/>
+  <template v-for="name in names" :key="name" > 
+    <h2 v-if="name === 'Bruce'">{{name}} </h2>
   </template>
+
 </template>
 
 <script>
@@ -22,27 +12,6 @@ export default {
   data(){
     return {
       names: ['Bruce','Clark','Diana'],
-      fullNames:[
-        {first:'Bruce',last:'Wayne'},
-        {first:'Clark',last:'Kent'},
-        {first:'Princess',last:'Diana'},
-      ],
-      actors: [
-        {
-          name: 'Christian Bale',
-          movies: ['Batman', 'The Prestige']
-        },
-        {
-          name: 'Di Caprio',
-          movies: ['Titanic', 'Inception']
-        },
-      ],
-
-      myInfo: {
-        name: "Raymond",
-        channel: "Full-stack",
-        course: "vue"
-      }
     }
   }
 }
