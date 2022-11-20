ce05871e7d1f9cd132d6b6db96571eefffa88439
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-20 11:29:09 -0700

    list Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,4 @@
 <template>
-  <h2 v-if="num === 0 "> The number is zero {{num}}</h2>
-  <h2 v-else-if="num < 0"> The number is negitive {{num}} </h2>
-  <h2 v-else> The number is not zero {{num}}</h2>
-  <template v-if="display">
-    <h2> Raymond </h2>
-    <h2> Code</h2>
-    <h2> Vye</h2>
-  </template>
-
-  
-  <h2 v-show="showElement"> Using v-show</h2>
-  <h2 v-if="ifShow"> useing v-if</h2>
 
 </template>
 
@@ -21,10 +9,6 @@ export default {
   name: 'App',
   data(){
     return {
-      num: -1,
-      display: true,
-      showElement:false,
-      ifShow:true
     }
   }
 }
