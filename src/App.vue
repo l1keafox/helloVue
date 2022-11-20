03d39b96cab693d2d4d70e441e906e5e1133fc64
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-19 21:07:56 -0700

    shorthand

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,25 @@
   <h2 class="underline" :class="status"> Status</h2>
   <h2 :class = "isPromoted && 'prompted'"> Promoted Movie </h2>
   <h2 :class="isSoldout ? 'sold-out' : 'new'"> Soldout? movie</h2> 
-  <h2 :class="['new','prompted']"> Newly promoted Moive</h2>
+  <h2 v-bind:class="['new','prompted']"> Newly promoted Moive</h2>
   <h2 :class="[isPromoted && 'prompted', isSoldout ? 'sold-out' : 'new' ]"> Array conditional movie</h2>
+
+  <h2 :class="{
+    prompted: isPromoted,
+    new: !isSoldout,
+    'sold-out': isSoldout
+  }"> Object conditional movie</h2>
+
+  <h2 v-bind:style="{
+    color: highlightcolor,
+    'font-size':headerSize,
+    fontSize:headerSize,
+    padding: '2px'
+  }"> Inline style </h2>
+
+  <h2 :style="headerStyleObject"> Style Object </h2>
+  <div :style="[baseStyleObject,successStyleObject]">sucess style</div> 
+  <div :style="[baseStyleObject,dangerStyleObject]"> Danger Style </div>
 </template>
 
 <script>
@@ -18,7 +35,29 @@ export default {
       status: 'danger',
       isDisabled:false,
       isPromoted: true,
-      isSoldout: true
+      isSoldout: true,
+      highlightcolor: 'orange',
+      headerSize: '50px',
+      headerStyleObject: {
+        fontSize:'20px',
+        color: 'purple',
+        padding: '20px'
+      },
+      baseStyleObject:{
+        fontSize: '50px',
+        padding: '10px'
+      },
+      successStyleObject:{
+        color: 'green',
+        backgroundColor: 'lightgreen',
+        border:'1px solid green',
+        padding: '20px'
+      },
+      dangerStyleObject: {
+        color: 'darkred',
+        backgroundColor:'red',
+        border:'1px solid darkred'
+      }
     }
   }
 }
