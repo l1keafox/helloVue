d0601c6a193d2862518e10f8bd389c1624632039
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-21 11:10:21 -0700

    Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <div class="flex flex-col justify-center items-center">
-  <h2>Fullname - {{firstName}} {{lastName}} </h2>
-  <h2> Computed Fullname - {{fullName}}</h2>
-  <button @click="changeFullName">  Change Fullname</button>
-
-
-  <h2> Totals - {{ items.reduce((total,curr) => (total=total+curr.price),0) }}</h2>
-  <button @click="items.push({id:items.length+1, title:'keyboard',price:50 } )"> BTM </button>
-  <h3> Total Computed {{calculateTotal}}</h3>
-  <h2> Method Total - {{getTotal() }} </h2>
-  <input type="text" v-model="country">
-
-  <template v-for="item in items" :key="item.id"> 
-    <h2 v-if="item.price > 100" >{{item.title}} $$ {{item.price}}</h2>
-  </template>
-
-  <h2 v-for="item in expensiveItems" :key="item.id"> 
-    {{item.title}} :: ? {{item.price}}
-  </h2>
-
+<div class = "flex flex-col justify-center items-center">
+  <h2> Volume Tracker (0-20)</h2>
+  <h3> current Volume: {{volume}}</h3>
+  <div class="flex ">
+    <button @click="volume+=2" class="bg-slate-400 border-2 border-sky-300 p-2 w-80"> Increase </button>
+    <button @click="volume-=2" class="bg-slate-400 border-2 border-red-300 p-2 w-80 ml-2"> Decrease </button>
+  </div>
 </div>
 </template>
 
@@ -28,57 +15,36 @@ export default {
   name: "App",
   data() {
     return {
-      firstName: 'Bruce',
-      lastName: 'Wayne',
-      items:[
-        {
-          id:1,
-          title: 'TV',
-          price:100
-        },
-        {
-          id:2,
-          title: 'Phone',
-          price:200
-        },
-        {
-          id:3,
-          title: 'Laptop',
-          price:300
-        },
-      ],
-      country:''
+      volume:0
     };
   },
   methods: {
     // Will always be changed in the UI.
-    getTotal(){
-      console.log('gettotal method');
-      return this.items.reduce((total,curr) => (total=total+curr.price),0)
-    },
-    changeFullName(){
-      this.fullName = "Johan Lee"
-    }
   },
   computed:{
     // computed is cached, and doesn't always get called when UI.
     // Filtering sorting here is best! Prevent Vue from recalcuating update doesn't concern table data.
-    fullName:{
-      get(){
-        return `${this.firstName} ${this.lastName}`
-      },
-      set(value){
-        const names = value.split(' ');
-        this.firstName = names[0];
-        this.lastName = names[1];
+    // Best to use when compose new data from existing data.
+    // Also used to reduce the length of a var - 
+    // Deeply nested property to bind it.
+
+  },
+  watch: { // This is like useEffect
+    // Generic way to react to reac tto data changes.
+    // Not recommend to use computed as a watcher.
+    // This will return the new value, and old value always
+    // Use watches to check to favorable vaue to know if you ready to perfomr an action.
+    // Common use case, call an api in response to data change.
+    // Use transitions, because it gives you the start and end state!
+    
+    volume(newValue,oldValue){
+      if(newValue === 16 && newValue > oldValue){
+        alert(
+          'DO NOT LISTEN TO HIGH VOLUME!'
+        )
       }
-    },
-    calculateTotal(){
-      console.log("get total calcuated")
-      return this.items.reduce((total,curr) => (total=total+curr.price),0)
-    },
-    expensiveItems(){
-      return this.items.filter(e=>e.price > 100);
+
+      if(this.volume > 20) this.volume = 20;
     }
   }
 };
@@ -91,5 +57,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+
 
 </style>
