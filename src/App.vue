fff255000aff2cd304af89e88b1105df245d67aa
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-20 21:52:10 -0700

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <h1 v-once>{{name}}</h1>
-  <button @click="name= 'batman'">Change Name</button>
-
-  <h2 v-pre>{{name}}</h2>
+  <div class="flex flex-col justify-center items-center">
+  <h2>Fullname - {{firstName}} {{lastName}} </h2>
+  <h2> Computed Fullname - {{fullName}}</h2>
+  <h2> Totals - {{ items.reduce((total,curr) => (total=total+curr.price),0) }}</h2>
+  <button @click="items.push({id:items.length+1, title:'keyboard',price:50 } )"> BTM </button>
+  <h3> Total Computed {{calculateTotal}}</h3>
+  <h2> {{items.map( e=> e )}} </h2>
+</div>
 </template>
 
 <script>
@@ -10,11 +14,37 @@ export default {
   name: "App",
   data() {
     return {
-      name:'boss'
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+      items:[
+        {
+          id:1,
+          title: 'TV',
+          price:100
+        },
+        {
+          id:2,
+          title: 'Phone',
+          price:120
+        },
+        {
+          id:3,
+          title: 'Laptop',
+          price:105
+        },
+      ]
     };
   },
   methods: {
   },
+  computed:{
+    fullName(){
+      return `${this.firstName} ${this.lastName}`
+    },
+    calculateTotal(){
+      return this.items.reduce((total,curr) => (total=total+curr.price),0)
+    }
+  }
 };
 </script>
 
@@ -25,9 +55,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-#formDiv {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+
 </style>
