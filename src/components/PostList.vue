bbe11bcf103e8c30dd29597a8e2411e4d896466e
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-22 20:49:53 -0700

    Axios failure

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <button @click="getPosts" class="border-2 border-yellow-500 p-3 w-96 bg-slate-400"> Post List </button>
+        <h3 v-if="errorMsg">{{errorMsg}} </h3>
+        <div v-for="post in posts" :key="post.id">
+            <h1 class="font-bold"> {{post.id}} {{post.title}}</h1>
+            <p> {{post.body}} </p>
+            <hr/>
+        </div>
     </div>
 </template>
 
@@ -12,12 +18,17 @@ import axios from 'axios'
         data(){
             return {
                 posts:[],
+                errorMsg:''
             }
         },
         methods:{
             async getPosts(){
-                let result = await axios.get('https://jsonplaceholder.typicode.com/posts');
-                console.log(result);
+                try{
+                    let result = await axios.get('https://jsonplaceholder.typicode.com/postsxx');
+                    this.posts = result.data;
+                }catch(err){
+                    this.errorMsg = err;
+                }
             }
         }
     }
