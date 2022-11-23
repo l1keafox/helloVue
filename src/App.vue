1ff63da0e56c7b32166a47d4b161da6bf477c7ee
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-23 15:55:21 -0700

    Life cycle hooks in composition

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col justify-center items-center pt-10">
-    <ProvideInject />
+    <LifeCycleC />
   </div>
 </template>
 
 <script>
-import ProvideInject from "./components/ProvideInject";
+import LifeCycleC from "./components/LifeCycleC";
 export default {
   name: "App",
 
   components: {
-    ProvideInject,
+    LifeCycleC,
   },
   setup(){
     // Composition API
---
+++ src/components/LifeCycleC.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import{
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted
+}from 'vue'
+    export default {
+        name:"LifeCycleC",
+        setup(){
+            onBeforeMount(()=>{
+                console.log('lifeCycleC on before onBeforeMount');
+            })
+            onMounted(()=>{
+                console.log('lifeCycleC on before onMounted');
+            })
+
+            onBeforeUpdate(()=>{
+                console.log('lifeCycleC on before onBeforeUpdate');
+            })
+            onUpdated(()=>{
+                console.log('lifeCycleC on before onUpdated');
+            })
+
+            onBeforeUnmount(()=>{
+                console.log('lifeCycleC on before onBeforeUnmount');
+            })
+            onUnmounted(()=>{
+                console.log('lifeCycleC on before onUnmounted');
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
